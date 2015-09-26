aca37ed380ff53a0ba707b17574d82c50cfe5062
pmqs <pmqs@cpan.org>
2015-09-26 18:41:46 +0100

    import Compress-Raw-Lzma 2.069
      2.069 26 Sep 2015
    
          * No Changes
    
    
    git-cpan-module:   Compress-Raw-Lzma
    git-cpan-version:  2.069
    git-cpan-authorid: pmqs

--- lib/Compress/Raw/Lzma.pm
+++ lib/Compress/Raw/Lzma.pm
@@ -12,7 +12,7 @@ use Carp ;
 # use bytes ;
 use vars qw($VERSION $XS_VERSION @ISA @EXPORT $AUTOLOAD);
 
-$VERSION = '2.068';
+$VERSION = '2.069';
 $XS_VERSION = $VERSION; 
 $VERSION = eval $VERSION;
 
@@ -1670,7 +1670,7 @@ See the Changes file.
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (c) 2005-2014 Paul Marquess. All rights reserved.
+Copyright (c) 2005-2015 Paul Marquess. All rights reserved.
 
 This program is free software; you can redistribute it and/or
 modify it under the same terms as Perl itself.
